833ea663af580f77083bf11ee202fbe0f028680d
venkat-giri <giri.ragav@gmail.com>
2017-06-13 21:13:09 -0500

    Added a client

---
+++ TestWCFService/MyClient/Default.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MyServiceReference;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        MyServiceClient srvcClient = new MyServiceClient();
+        Label1.Text = srvcClient.GetData(Int32.Parse(TextBox1.Text));
+    }
+}
